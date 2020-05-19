9d3496aad8ee14c5e32568648c21a7f47b4d7266
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-18 22:12:14 -0700

    Learn basic form input validation

--- index.php
+++ index.php
@@ -9,25 +9,35 @@
 
 <body>
 
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    Name: <input type="text" name="fname">
-    <input type="submit">
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    Name: <input type="text" name="name">
+    E-mail: <input type="text" name="email">
+    Website: <input type="text" name="website">
+    Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+    Gender:
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
   </form>
 
   <?php
+  // define variables and set to empty values
+  $name = $email = $gender = $comment = $website = "";
+
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = $_REQUEST['fname'];
-    echo "<br>";
-    print_r($_REQUEST);
-    echo "<br>";
-    print_r($_POST);
-    echo "<br>";
-    if (empty($name)) {
-      echo "Name is empty";
-    } else {
-      echo $name;
-    }
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $website = test_input($_POST["website"]);
+    $comment = test_input($_POST["comment"]);
+    $gender = test_input($_POST["gender"]);
+  }
+
+  function test_input($data)
+  {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
   }
   ?>
 

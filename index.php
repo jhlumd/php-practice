09d3d0e8c96d691eb383ca620b22af42456333db
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-18 20:34:49 -0700

    Learn array

--- index.php
+++ index.php
@@ -10,19 +10,20 @@
 <body>
 
   <?php
-  $array = array(
-    "foo" => "bar",
-    42    => 24,
-    "multi" => array(
-      "dimensional" => array(
-        "array" => "foo"
-      )
-    )
-  );
+  $arr = array(5 => 1, 12 => 2);
 
-  var_dump($array["foo"]);
-  var_dump($array[42]);
-  var_dump($array["multi"]["dimensional"]["array"]);
+  $arr[] = 56;    // This is the same as $arr[13] = 56;
+  // at this point of the script
+
+  $arr["x"] = 42; // This adds a new element to
+  // the array with key "x"
+
+  unset($arr[5]); // This removes the element from the array
+  var_dump($arr);
+
+  unset($arr);    // This deletes the whole array
+  echo "<br>";
+  var_dump($arr);
   ?>
 
 </body>

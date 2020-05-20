384f758ff60286507593d08f38adbfdebda0c3b3
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-19 17:02:46 -0700

    Add section for past tests and gitignore

--- index.php
+++ index.php
@@ -11,47 +11,44 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// // Create database
-// $sql = "CREATE DATABASE ninja_pizza_2";
-// if (mysqli_query($conn, $sql)) {
-//   echo "Database created successfully";
-// } else {
-//   echo "Error creating database: " . mysqli_error($conn);
-// }
+// Create database
+$sql = "CREATE DATABASE ninja_pizza_2";
+if (mysqli_query($conn, $sql)) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . mysqli_error($conn);
+}
 
-// // sql to create table
-// $sql = "CREATE TABLE pizzas (
-// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-// title VARCHAR(255) NOT NULL,
-// ingredients VARCHAR(255) NOT NULL,
-// email VARCHAR(255) NOT NULL,
-// created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-// )";
+// sql to create table
+$sql = "CREATE TABLE pizzas (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+ingredients VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
-// if (mysqli_query($conn, $sql)) {
-//   echo "Table pizzas created successfully";
-// } else {
-//   echo "Error creating table: " . mysqli_error($conn);
-// }
+if (mysqli_query($conn, $sql)) {
+  echo "Table pizzas created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
 
-// // Insert data in to table
-// $sql = "INSERT INTO pizzas (title, ingredients, email)
-// VALUES ('nina supreme 2', 'tomato2, cheese2, tofu2', 'john@example.com')";
-// if (mysqli_query($conn, $sql)) {
-//   echo "New record created successfully";
-// } else {
-//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-// }
-// $sql = "INSERT INTO pizzas (title, ingredients, email)
-// VALUES ('mario supreme 2', 'tomato2, cheese2, mushroom2', 'mario@example.com')";
-// if (mysqli_query($conn, $sql)) {
-//   echo "New record created successfully";
-// } else {
-//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-// }
-
-
-
+// Insert data in to table
+$sql = "INSERT INTO pizzas (title, ingredients, email)
+VALUES ('nina supreme 2', 'tomato2, cheese2, tofu2', 'john@example.com')";
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+$sql = "INSERT INTO pizzas (title, ingredients, email)
+VALUES ('mario supreme 2', 'tomato2, cheese2, mushroom2', 'mario@example.com')";
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
 // write query for all pizzas
 $sql = "SELECT title, ingredients, id FROM pizzas ORDER BY created_at";
